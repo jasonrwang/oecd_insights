c08bb5f3b0586a22f5d7770289c3062d7b3c9767
Skeij <r.s.keij@gmail.com>
2018-10-25 14:35:07 +0200

    try

--- best_countries.R
+++ best_countries.R
@@ -16,38 +16,38 @@ health_expenditure <- read.csv("data/Health_expenditure(GDP).csv",sep=',',header
 household_consumption <- read.csv("data/Household_consumption(US).csv",sep=',',header=TRUE)
 # https://data.worldbank.org/indicator/NE.CON.PRVT.CD
 country_names <- read.csv("data/Metadata_Country.csv",sep=',',header=TRUE)
+header_names <- c('Country_Name', 'Country_Code', 'Indicator_Name','Indicator_Code',1960:2017)
 
-# doesn't work from list for some reason
+# data_frames <- list(age_dependency_old,age_dependency_young,age_dependency_ratio,GDP,health_expenditure_capita,health_expenditure,household_consumption)
+
+length(data_frames)
+
+for (i in 1:length(data_frames)) {
+    data_frames[[i]][length(data_frames[[i]])] <- NULL
+    colnames(data_frames[[i]]) <- header_names
+}
+
+DataFramesClean <- function (...) {
+    DataFrames <- list(...)
+    for (i in DataFrames) {
+        i[length(i)] <- NULL
+        colnames(i) <- header_names
+        print(colnames(i))
+        return(i)
+    }
+    # print(...)
+    # for (i in ...)) {
+    #     i[length(i)] <- NULL
+    #     colnames(i) <- header_names
+    #     # return(i)
+    # }
+}
+
+DataFramesClean(age_dependency_old,age_dependency_ratio,age_dependency_young)
+colnames(age_dependency_old)
+
+DataFramesClean(age_dependency_old,age_dependency_young,age_dependency_ratio,GDP,health_expenditure_capita,health_expenditure)
+colnames(age_dependency_old)
+
 age[length(age_dependency_old)] <- NULL
 colnames(age_dependency_old) <- header_names
-
-age_dependency_young[length(age_dependency_young)] <- NULL
-colnames(age_dependency_young) <- header_names
-
-age_dependency_ratio[length(age_dependency_ratio)] <- NULL
-colnames(age_dependency_ratio) <- header_names
-
-GDP[length(GDP)] <- NULL
-colnames(GDP) <- header_names
-
-health_expenditure_capita[length(health_expenditure_capita)] <- NULL
-colnames(health_expenditure_capita) <- header_names
-
-health_expenditure[length(health_expenditure)] <- NULL
-colnames(health_expenditure) <- header_names
-
-age_dependency_old[length(age_dependency_old)] <- NULL
-colnames(age_dependency_old) <- header_names
-
-household_consumption[length(household_consumption)] <- NULL
-colnames(household_consumption) <- header_names
-
-
-# for (i in data_frames) {
-#     print(typeof(i))
-#     # print(dim(i)[2])
-#     # i[length(i)] <- NULL
-#     # print(dim(i)[2])
-#     colnames(i) <- header_names
-# }
-# colnames(age_dependency_old)
