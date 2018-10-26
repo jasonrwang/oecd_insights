48767a95bb86e25bcdf44027d25977bb0552be00
Skeij <r.s.keij@gmail.com>
2018-10-26 14:32:22 +0200

    Added comments to code.

--- getdata.R
+++ getdata.R
@@ -1,3 +1,4 @@
+#import all the different dataframes
 age_dependency_old <- read.csv("data/API_SP.POP.DPND.OL_DS2_en_csv_v2_10184911.csv",sep=',',header=TRUE,skip=4)
 age_dependency_young <- read.csv("data/API_SP.POP.DPND.YG_DS2_en_csv_v2_10199819.csv",sep=',',header=TRUE,skip=4)
 age_dependency_ratio <- read.csv("data/API_SP.POP.DPND_DS2_en_csv_v2_10188478.csv",sep=',',header=TRUE,skip=4)
@@ -6,13 +7,19 @@ health_expenditure_capita <- read.csv("data/API_SH.XPD.EHEX.PC.CD_DS2_en_csv_v2_
 health_expenditure <- read.csv("data/API_SH.XPD.CHEX.GD.ZS_DS2_en_csv_v2_10183765.csv",sep=',',header=TRUE,skip=4)
 household_consumption <- read.csv("data/API_NE.CON.PRVT.CD_DS2_en_csv_v2_10184156.csv",sep=',',header=TRUE,skip=4)
 
+#create a list of all the names of the dataframes and the generic header names we want
 list_of_data_frames <- c('age_dependency_old','age_dependency_young','age_dependency_ratio','GDP','health_expenditure_capita','health_expenditure','household_consumption')
 header_names <- c('Country_Name', 'Country_Code', 'Indicator_Name','Indicator_Code',1960:2017)
 
 CDF <- function(D) {
-    D[length(D)] <- NULL
-    colnames(D) <- header_names
+    D[length(D)] <- NULL #delete the last column, it is empty
+    colnames(D) <- header_names #change the headernames to be the same
     return(D)
 }
 
-assign(list_of_data_frames[1], CDF(get(list_of_data_frames[1])))
+#walk through the list and change it according to the function
+for (i in 1:length(list_of_data_frames)) {
+    assign(list_of_data_frames[i], CDF(get(list_of_data_frames[i])))
+}
+
+# View(age_dependency_old)
