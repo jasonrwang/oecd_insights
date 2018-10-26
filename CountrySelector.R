4b7a92ecf7a0d918aeeec46468244e9a0d525e94
Philip Seijger <philipseijger@l-145-118-235-138.leidenuniv.nl>
2018-10-26 14:21:52 +0200

    Country selector based on combined NA's

---
+++ CountrySelector.R
@@ -0,0 +1,11 @@
+# Function to select 50 countries with fewest NA's across multiple dataframes
+
+CountrySelector <- function(...) {
+  DataFrames <- list(...)  # list all dataframes fed to CountrySelector
+  l2 <- lapply(DataFrames, function(x) cbind(x, na_count = rowSums(is.na(x))))  # Add column with NA-value count per row
+  l3 <- Reduce(rbind, l2)  # Create one big dataframe
+  l4 <- aggregate(x = l3$na_count, by = list(l3$Country.Name), FUN = sum)  # Combined NA-value counts for all dataframes per country
+  l5 <- l4[order(l4$x),]  # Order ascending
+  CountryList <- list(head(l5$Group.1, 50))  # List 50 countries with lowest NA-value counts
+  return(CountryList)
+}
