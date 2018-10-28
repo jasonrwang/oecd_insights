85b65a1f5e23c1919b6bb0e1e1931aad765c111f
jasonrwang <accounts@jrwang.ca>
2018-10-28 17:27:57 +0100

    Renamed for filename consistency

--- plotCorrelations.R
+++ plotCorrelations.R
@@ -1,3 +1,8 @@
 ## This script plots correlations between the age dependency ratio and other indicators
 
+AllInds_cast[[ AllInds_cast['Indicator_Code'] == 'SP.POP.DPND' ]]
+# for indicator to compare to age
+    # for country
+        # for year
+
 plot()
