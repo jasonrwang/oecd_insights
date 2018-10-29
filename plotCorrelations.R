47071d4cab5a8a8af9f8e9faf4cb0bc1119b13c0
jasonrwang <accounts@jrwang.ca>
2018-10-29 10:16:08 +0100

    Now plots...something for #9. Now must subplot each indicator by country

--- plotCorrelations.R
+++ plotCorrelations.R
@@ -1,8 +1,21 @@
-## This script plots correlations between the age dependency ratio and other indicators
+### This script plots correlations between the age dependency ratio and other indicators
 
-AllInds_cast[[ AllInds_cast['Indicator_Code'] == 'SP.POP.DPND' ]]
+# These are the indicators we are concerned about
+indicatorsVect = c(
+    "NY.GDP.MKTP.CD","SH.XPD.EHEX.PC.CD","SH.XPD.CHEX.GD.ZS",
+    "NE.CON.PRVT.CD","SI.POV.GINI","SP.DYN.LE00.IN","SP.POP.TOTL")
+
+AllInds_cast['SP.POP.DPND']
 # for indicator to compare to age
     # for country
         # for year
 
-plot()
+# dim(AllInds_cast['SP.POP.DPND'])
+# dim(AllInds_cast[indicator])
+
+for (indicator in indicatorsVect) { # Ignore the three age dependency indicators
+    # new subplot
+    # if countries are separated (maybe they should be?), then call maybe subset each country?
+    plot(unlist(AllInds_cast['SP.POP.DPND']),unlist(AllInds_cast[indicator]))
+}
+
