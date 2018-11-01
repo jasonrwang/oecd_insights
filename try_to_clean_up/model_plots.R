654c8ad96607cd0a9af3256a78896e365ed168ef
Skeij <r.s.keij@gmail.com>
2018-11-01 22:17:16 +0100

    tried to make the final code leaner

---
+++ try_to_clean_up/model_plots.R
@@ -0,0 +1,17 @@
+library(rjags)
+# samp=readRDS("fork.RDS")
+samp=readRDS("chain.RDS")
+
+summary(samp)
+traceplot(samp)
+
+# sometimes the gelman plot won't fit on a screen
+# we havve to reduce the margins
+par(mar=c(.4,.4,.4,.4))
+gelman.plot(samp)
+gelman.diag(samp)
+densplot(samp)
+acfplot(samp)
+
+# get the effective sample size
+effectiveSize(samp)
